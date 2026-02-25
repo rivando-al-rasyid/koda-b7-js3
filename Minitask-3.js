7cce78a6c8474697628c1b75f4965507afb6d759
Rivando Al rasyid <119521535+rivando-al-rasyid@users.noreply.github.com>
2026-02-25 15:55:06 +0700

    Add files via upload

---
+++ Minitask-3.js
@@ -0,0 +1,30 @@
+
+kembalian = 155000
+uang50Rb = 0
+uang10Rb = 0
+uang5Rb = 0
+uang1Rb = 0
+
+    while(kembalian > 0){
+    if (kembalian > 50000){
+            kembalian = kembalian - 50000
+            uang50Rb++
+        }
+    else if (kembalian >= 10000 && kembalian < 50000 ){
+            kembalian = kembalian - 10000
+            uang10Rb++
+        }
+    else if (kembalian >= 5000 && kembalian < 10000 ){
+            kembalian = kembalian - 5000
+            uang5Rb++
+        }
+    else if (kembalian >= 1000 && kembalian < 10000 ){
+            kembalian = kembalian - 1000
+            uang1Rb++
+    }
+
+}
+    console.log('kembalian 50Rb = '+ uang50Rb)
+    console.log('kembalian 10Rb = '+ uang10Rb)
+    console.log('kembalian 5Rb = '+ uang5Rb)
+    console.log('kembalian 1Rb = '+ uang1Rb)
